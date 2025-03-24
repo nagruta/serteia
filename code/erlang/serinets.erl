81870344ccaa7d352a0b50dfbb498b743f23a401
Christopher Augustus <repos@c4augustus.com>
2025-03-24 12:41:20 +0100

    enhance [melhorar] serinets into an acquirable singleton process instead of only for [num processo singleton o que é adquirível, em vez de só para] escript

--- code/erlang/serinets.erl
+++ code/erlang/serinets.erl
@@ -1,14 +1,24 @@
--export([main/1]).
+-module(serinets).
+-export([acquire/0, init/0]).
+-define(PROCESS, list_to_atom(atom_to_list(?MODULE) ++ "_single")).
 
-main(_) ->
-  ok = inets:start(),
-  {ok, _} = inets:start(httpd, [
+acquire() ->
+  case whereis(?PROCESS) of
+    Pid when is_pid(Pid) -> Pid;
+    _ -> spawn(?MODULE, init, [])
+  end.
+
+init() ->
+  ok = application:ensure_started(inets),
+  {ok,PidHttpd} = inets:start(httpd, [
     {server_name,   "serteia"   },
     {server_root,   "."         },
     {document_root, "."         },
     {bind_address,  "localhost" },
     {port,          8088        }
   ]),
+  register(?PROCESS, self()),
   receive
-    stop -> ok
+    stop -> inets:stop(httpd, PidHttpd), ok;
+    stop_inets -> inets:stop(), ok
   end.
