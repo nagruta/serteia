d26e833bff50910cd7c04029790e12365bd93f0b
Christopher Augustus <repos@c4augustus.com>
2025-04-04 23:13:38 +0100

    rewrite acquire/2 and create/2 so that both the address/hostname and port can be provided

--- code/erlang/serinets.erl
+++ code/erlang/serinets.erl
@@ -6,7 +6,7 @@
 %%  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %%
 -module(serinets).
--export([acquire/0, create/1, init/1, do/1]).
+-export([acquire/0, acquire/1, acquire/2, create/2, init/2, do/1]).
 
 -include_lib("kernel/include/file.hrl").
 -include_lib("inets/include/httpd.hrl").
@@ -26,9 +26,6 @@
 %%  connection}).
 
 -define(NAME_SINGLETON    , list_to_atom(?MODULE_STRING++"_single")).
--define(HTTPD_ADDR_LOCAL  , "localhost"                 ).
--define(HTTPD_PORT_LOCAL  , 8088                        ).
--define(HTTPD_PORT_PUBLIC , 4433                        ).
 -define(PATH_MODULE       , filename:dirname(proplists:get_value(source, module_info(compile)))).
 -define(PATH_CODE         , ?PATH_MODULE++"/.."         ).
 -define(PATH_TOP          , ?PATH_CODE++"/.."           ).
@@ -40,25 +37,21 @@
 -define(PATH_SITE_HERE    , ?PATH_TOP++"/site"          ).
 -define(PATH_SITE_THERE   , ?PATH_TOP++"/../site"       ).
 
-acquire() ->
+acquire()           -> acquire("localhost", 8088).
+acquire(Addr)       -> acquire(Addr       , 443).
+acquire(Addr, Port) ->
   case whereis(?NAME_SINGLETON) of
     Pid when is_pid(Pid) -> Pid;
-    _ -> create(local)
+    _ -> create(Addr, Port)
   end.
 
-create(Addr) ->
+create(Addr, Port) ->
   case whereis(?NAME_SINGLETON) of
     Pid when is_pid(Pid) -> {error,already_exists_at_pid,Pid};
-    _ -> spawn(?MODULE, init, [Addr])
+    _ -> spawn(?MODULE, init, [Addr, Port])
   end.
 
-init(Addr) ->
-  case Addr of
-    local -> p_start(?HTTPD_ADDR_LOCAL, ?HTTPD_PORT_LOCAL );
-    _     -> p_start(Addr             , ?HTTPD_PORT_PUBLIC)
-  end.
-
-p_start(Addr, Port) ->
+init(Addr, Port) ->
   p_report("starting this process "++pid_to_list(self())),
   Sites = lists:append(p_subdirs(?PATH_SITE_HERE),
                        p_subdirs(?PATH_SITE_THERE)),
@@ -92,27 +85,31 @@ p_start(Addr, Port) ->
         p_report("No HTTPS support; missing cert files")
   end,
   ok = application:ensure_started(inets),
-  {ok,PidHttpd} = inets:start(httpd, [
-     {bind_address    , Addr                        }
-    ,{document_root   , ?PATH_SITE_HERE             }
-    ,{port            , Port                        }
-    ,{server_name     , Addr                        }
-    ,{server_root     , ?PATH_TOP                   }
-    ,SocketType
-    ,{modules         , [serinets, mod_get, mod_log]}
-    %% mod_log config:
-    ,{error_log       , ?DIR_LOG++"/error.log"      }
-    ,{security_log    , ?DIR_LOG++"/security.log"   }
-    ,{transfer_log    , ?DIR_LOG++"/transfer.log"   }
-  ]),
-  p_report("started inets httpd process "++pid_to_list(PidHttpd)),
-  register(?NAME_SINGLETON, self()),
-  p_loop(PidHttpd, Addr, Port).
+  case inets:start(httpd, [
+         {bind_address  , Addr                        }
+        ,{document_root , ?PATH_SITE_HERE             }
+        ,{port          , Port                        }
+        ,{server_name   , Addr                        }
+        ,{server_root   , ?PATH_TOP                   }
+        ,SocketType
+        ,{modules       , [serinets, mod_get, mod_log]}
+        %% mod_log config:
+        ,{error_log     , ?DIR_LOG++"/error.log"      }
+        ,{security_log  , ?DIR_LOG++"/security.log"   }
+        ,{transfer_log  , ?DIR_LOG++"/transfer.log"   }
+      ]) of
+    {ok,PidHttpd} ->
+      p_report("started inets httpd process "++pid_to_list(PidHttpd)),
+      register(?NAME_SINGLETON, self()),
+      p_loop(PidHttpd, Addr, Port);
+    Failure ->
+      p_report(["failed to start inets httpd: ", Failure])
+  end.
 
 p_loop(PidHttpd, Addr, Port) ->
   receive
     {report,Info} -> p_report(Info),    p_loop(PidHttpd, Addr, Port);
-    restart       -> p_stop(PidHttpd),  p_start(Addr, Port);
+    restart       -> p_stop(PidHttpd),  init(Addr, Port);
     stop          -> p_stop(PidHttpd),  ok;
     stop_inets    -> inets:stop(),      ok
   end.
